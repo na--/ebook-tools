d5d25ba28d9d032f17de988f8992432b77ddf913
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2017-10-12 17:02:24 +0300

    Update get-isbn.sh

--- get-isbns.sh
+++ get-isbns.sh
@@ -6,7 +6,16 @@ DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 # shellcheck source=./lib.sh
 . "$DIR/lib.sh"
 
-ISBN_RET_SEPARATOR="\n"
+ISBN_RET_SEPARATOR=$'\n'
+
+for arg in "$@"; do
+	case $arg in
+		-*|--*) handle_script_arg "$arg" ;;
+		*) break ;;
+	esac
+	shift # past argument=value or argument with no value
+done
+unset -v arg
 
 #TODO: add options, help
 #TODO: add option to limit the number of ISBNs?
