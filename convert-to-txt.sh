08028204a479db3e896cd88f0bc39e1b887f3863
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2017-11-20 01:54:55 +0200

    Fix indentation

--- convert-to-txt.sh
+++ convert-to-txt.sh
@@ -32,35 +32,35 @@ mime_type=$(file --brief --mime-type "$input_file")
 is_tmp_file=false
 
 if [[ "$output_file" == "" ]]; then
-    is_tmp_file=true
-    output_file=$(mktemp --suffix='.txt')
-    decho "Created a temporary file '$output_file'"
+	is_tmp_file=true
+	output_file=$(mktemp --suffix='.txt')
+	decho "Created a temporary file '$output_file'"
 elif [[ "${output_file##*.}" != "txt" ]]; then
-    echo "Error: the output file needs to have a .txt extension!"
-    exit 1
+	echo "Error: the output file needs to have a .txt extension!"
+	exit 1
 fi
 args=("$input_file" "$output_file" "$mime_type")
 
 if [[ "$OCR_ENABLED" == "always" ]]; then
-    decho "OCR=always, first try OCR then conversion"
-    if ! ocr_file "${args[@]}"; then
-        convert_to_txt "${args[@]}"
-    fi
+	decho "OCR=always, first try OCR then conversion"
+	if ! ocr_file "${args[@]}"; then
+		convert_to_txt "${args[@]}"
+	fi
 elif [[ "$OCR_ENABLED" == "true" ]]; then
-    decho "OCR=true, first try conversion and then OCR"
-    if convert_to_txt "${args[@]}" && grep -qiE "[[:alnum:]]+" "$output_file"; then
-        decho "conversion successful, will not try OCR"
-    else
-        ocr_file "${args[@]}"
-    fi
+	decho "OCR=true, first try conversion and then OCR"
+	if convert_to_txt "${args[@]}" && grep -qiE "[[:alnum:]]+" "$output_file"; then
+		decho "conversion successful, will not try OCR"
+	else
+		ocr_file "${args[@]}"
+	fi
 else
-    decho "OCR=false, try only conversion"
-    convert_to_txt "${args[@]}"
+	decho "OCR=false, try only conversion"
+	convert_to_txt "${args[@]}"
 fi
 
 if [[ "$is_tmp_file" == true ]]; then
-    cat "$output_file"
+	cat "$output_file"
 
-    decho "Removing the temporary file '$output_file'"
-    rm "$output_file"
+	decho "Removing the temporary file '$output_file'"
+	rm "$output_file"
 fi
--- rename-calibre-library.sh
+++ rename-calibre-library.sh
@@ -24,57 +24,57 @@ if [[ "$#" == "0" ]]; then
 fi
 
 gm() {
-    python2 -c "from lxml.etree import parse; from sys import stdin; print((u'\\n'.join(parse(stdin).xpath('$2'))).encode('utf-8'))" < "$1"
+	python2 -c "from lxml.etree import parse; from sys import stdin; print((u'\\n'.join(parse(stdin).xpath('$2'))).encode('utf-8'))" < "$1"
 }
 
 find "$@" -type f ! -name "*.opf" ! -name "cover.jpg" -print0 | sort -z ${FILE_SORT_FLAGS[@]:+"${FILE_SORT_FLAGS[@]}"} | while IFS= read -r -d '' book_path
 do
-    metadata_path="$(dirname "$book_path")/metadata.opf"
-    if [[ ! -f "$metadata_path" ]]; then
-        decho "Skipping file '$book_path' - no metadata.opf present!"
-        continue
-    fi
+	metadata_path="$(dirname "$book_path")/metadata.opf"
+	if [[ ! -f "$metadata_path" ]]; then
+		decho "Skipping file '$book_path' - no metadata.opf present!"
+		continue
+	fi
 
-    decho "Found file '$book_path' with metadata.opf present, parsing metadata..."
+	decho "Found file '$book_path' with metadata.opf present, parsing metadata..."
 
-    declare -A d=( ["EXT"]="${book_path##*.}" )
-    d["TITLE"]=$(gm "$metadata_path" '//*[local-name()="title"]/text()')
-    d["AUTHORS"]=$(gm "$metadata_path" '//*[local-name()="creator"]/text()' | stream_concat ', ')
-    d["SERIES"]=$(gm "$metadata_path" '//*[local-name()="meta"][@name="calibre:series"]/@content')
-    if [[ "${d['SERIES']}" != "" ]]; then
-        d["SERIES"]="${d['SERIES']} #$(gm "$metadata_path" '//*[local-name()="meta"][@name="calibre:series_index"]/@content')"
-    fi
-    d["PUBLISHED"]=$(gm "$metadata_path" '//*[local-name()="date"]/text()')
-    d["ISBN"]=$(gm "$metadata_path" '//*[local-name()="identifier"][@*[local-name()="scheme" and .="ISBN"]]/text()')
+	declare -A d=( ["EXT"]="${book_path##*.}" )
+	d["TITLE"]=$(gm "$metadata_path" '//*[local-name()="title"]/text()')
+	d["AUTHORS"]=$(gm "$metadata_path" '//*[local-name()="creator"]/text()' | stream_concat ', ')
+	d["SERIES"]=$(gm "$metadata_path" '//*[local-name()="meta"][@name="calibre:series"]/@content')
+	if [[ "${d['SERIES']}" != "" ]]; then
+		d["SERIES"]="${d['SERIES']} #$(gm "$metadata_path" '//*[local-name()="meta"][@name="calibre:series_index"]/@content')"
+	fi
+	d["PUBLISHED"]=$(gm "$metadata_path" '//*[local-name()="date"]/text()')
+	d["ISBN"]=$(gm "$metadata_path" '//*[local-name()="identifier"][@*[local-name()="scheme" and .="ISBN"]]/text()')
 
-    if [[ "${d['ISBN']}" == "" ]]; then
-        d['ISBN']=$(find_isbns < "$metadata_path")
-    fi
+	if [[ "${d['ISBN']}" == "" ]]; then
+		d['ISBN']=$(find_isbns < "$metadata_path")
+	fi
 
-    decho "Parsed metadata:"
+	decho "Parsed metadata:"
 	for key in "${!d[@]}"; do
-        #TODO: fix this properly
-        d["$key"]=$(echo "${d[${key}]}" | sed -e 's/[\\/\*\?<>\|\x01-\x1F\x7F\x22\x24\x60]/_/g' | cut -c 1-100)
+		#TODO: fix this properly
+		d["$key"]=$(echo "${d[${key}]}" | sed -e 's/[\\/\*\?<>\|\x01-\x1F\x7F\x22\x24\x60]/_/g' | cut -c 1-100)
 		echo "${d[${key}]}" | debug_prefixer "    ${key}" 25
 	done
 
-    new_name="$(eval echo "$OUTPUT_FILENAME_TEMPLATE")"
-    new_path="$(unique_filename "${OUTPUT_FOLDER:-$(dirname "$book_path")}" "$new_name")"
+	new_name="$(eval echo "$OUTPUT_FILENAME_TEMPLATE")"
+	new_path="$(unique_filename "${OUTPUT_FOLDER:-$(dirname "$book_path")}" "$new_name")"
 	decho "Moving file to '$new_path'..."
 
-    move_or_link_file "$book_path" "$new_path"
-    case "$SAVE_METADATA" in
-        recreate)
-            {
-                echo "Title               : ${d['TITLE']}"
-                echo "Author(s)           : ${d['AUTHORS']}"
-                echo "Series              : ${d['SERIES']}"
-                echo "Published           : ${d['PUBLISHED']}"
-                echo "ISBN                : ${d['ISBN']}"
-                echo "Old file path       : $book_path"
-                echo "Metadata source     : metadata.opf"
-            } | grep -vE " : $" > "${new_path}.$OUTPUT_METADATA_EXTENSION"
-        ;;
+	move_or_link_file "$book_path" "$new_path"
+	case "$SAVE_METADATA" in
+		recreate)
+			{
+				echo "Title               : ${d['TITLE']}"
+				echo "Author(s)           : ${d['AUTHORS']}"
+				echo "Series              : ${d['SERIES']}"
+				echo "Published           : ${d['PUBLISHED']}"
+				echo "ISBN                : ${d['ISBN']}"
+				echo "Old file path       : $book_path"
+				echo "Metadata source     : metadata.opf"
+			} | grep -vE " : $" > "${new_path}.$OUTPUT_METADATA_EXTENSION"
+		;;
 		opfcopy) cp --no-clobber "$metadata_path" "${new_path}.$OUTPUT_METADATA_EXTENSION" ;;
 		*) decho "Metadata was not copied or recreated" ;;
 	esac
